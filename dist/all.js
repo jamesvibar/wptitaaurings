b52397f4d23847bb0931c0d51ccbedbc6ba020cc
James <james@redkite.com.ph>
2019-02-15 15:14:39 +0800

    Add a links to logo

--- dist/all.js
+++ dist/all.js
@@ -15369,7 +15369,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60471" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62019" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
